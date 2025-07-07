b07e288ad34fa8ee38adb1c047c580e21d8b45bc
Ansgar Hoyer <ansgar.hoyer@gmail.com>
2025-07-07 18:25:46 +0200

    feat: shadcn setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
+import { Button } from './components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <Button>Click me</Button>
   </StrictMode>,
 )
